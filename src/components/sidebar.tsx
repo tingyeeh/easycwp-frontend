8c6ea596089116d3c10a8b042a39759bc93b315b
Ting <ting.yeeh@outlook.com>
2023-03-28 23:02:47 +0800

    Delete i18n sub project

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import styles from '@/styles/sidebar.module.css'
+
+export default function Sidebar() {
+  return (
+    <nav className={styles.nav}>
+    This is sidebar
+    </nav>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { NextPage } from 'next'
+import { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '@/lib/apollo'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const apolloClient = useApollo(pageProps.initialApolloState)
+
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
