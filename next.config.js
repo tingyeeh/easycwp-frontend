5ff5fbe4609b7f2f3f9e5bab602218d71749541b
Ting <ting.yeeh@outlook.com>
2023-04-20 08:17:57 +0800

    restart again

--- next.config.js
+++ next.config.js
@@ -1,50 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    dirs: ['src'],
+  experimental: {
+    appDir: true,
   },
-  i18n: {
-    locales: [
-      'en', 
-      'ko', 
-      'ja', 
-      'vi', 
-      'fr', 
-      'de', 
-      'zh-Hans', 
-      'zh-Hant'
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
     ],
-    defaultLocale: 'en',
   },
+}
 
-  reactStrictMode: true,
-  swcMinify: true,
-
-  // Uncoment to add domain whitelist
-  // images: {
-  //   domains: [
-  //     'res.cloudinary.com',
-  //   ],
-  // },
-
-  // SVGR
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            typescript: true,
-            icon: true,
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
